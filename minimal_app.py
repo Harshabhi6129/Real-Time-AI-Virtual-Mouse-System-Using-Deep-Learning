782d7c5b01c4661d8d163755dc776a7fc702435e
HARSHA ABHINAV <140812124+Harshabhi6129@users.noreply.github.com>
2025-03-03 17:49:07 -0500

    Update minimal_app.py

--- minimal_app.py
+++ minimal_app.py
@@ -2,4 +2,15 @@ import streamlit as st
 from streamlit_webrtc import webrtc_streamer
 
 st.title("Minimal WebRTC Test")
-webrtc_streamer(key="test")
+webrtc_streamer(
+    key="test",
+    media_stream_constraints={
+        "video": {
+            "width": {"ideal": 320},
+            "height": {"ideal": 240},
+            "frameRate": {"ideal": 10}
+        },
+        "audio": False
+    }
+)
+
